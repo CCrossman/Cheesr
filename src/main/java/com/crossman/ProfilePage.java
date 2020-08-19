cee459261acb6b77d7095c9d9acd8d1efb92d16d
ccrossman <christopher.j.crossman@gmail.com>
2020-08-18 18:06:09 -0700

    Select address type when adding to profile.

--- src/main/java/com/crossman/ProfilePage.java
+++ src/main/java/com/crossman/ProfilePage.java
@@ -1,15 +1,22 @@
 package com.crossman;
 
 import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.markup.html.link.Link;
 import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.model.LoadableDetachableModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.Arrays;
+
 public final class ProfilePage extends CheesrPage implements IRequireAuthorization {
+	// used only by PropertyModel for DropDownChoice
+	private String addressType = "HOME";
 
 	public ProfilePage(PageParameters pageParameters) {
 		super(pageParameters);
@@ -22,14 +29,22 @@ public final class ProfilePage extends CheesrPage implements IRequireAuthorizati
 		final TextField tfState = new TextField("state", new Model());
 		final TextField tfZip = new TextField("zip", new Model());
 
-		Form form = new Form("form") {
+		final DropDownChoice ddc = new DropDownChoice("choices", new PropertyModel(this, "addressType"), new LoadableDetachableModel() {
+			@Override
+			protected Object load() {
+				return Arrays.asList(Address.Type.values());
+			}
+		});
+
+		final Form form = new Form("form") {
 			@Override
 			protected void onSubmit() {
 				// save address in database
 				final Sql2o sql2o = WicketApplication.getInjector().getInstance(Sql2o.class);
 				try (Connection c = sql2o.open()) {
-					long count = c.createQuery("SELECT count(*) FROM addresses WHERE username = :usr AND kind = 'HOME'")
+					long count = c.createQuery("SELECT count(*) FROM addresses WHERE username = :usr AND kind = :kind")
 							.addParameter("usr", getCheesrSession().getUsername())
+							.addParameter("kind", addressType)
 							.executeScalar(Long.class);
 
 					final String name = tfName.getModelObjectAsString();
@@ -39,18 +54,20 @@ public final class ProfilePage extends CheesrPage implements IRequireAuthorizati
 					final String zip = tfZip.getModelObjectAsString();
 
 					if (count == 0) {
-						c.createQuery("INSERT INTO addresses (username, kind, name, street, city, state, zip) VALUES (:usr, 'HOME', :name, :street, :city, :state, :zip)")
+						c.createQuery("INSERT INTO addresses (username, kind, name, street, city, state, zip) VALUES (:usr, :kind, :name, :street, :city, :state, :zip)")
 								.addParameter("usr", getCheesrSession().getUsername())
 								.addParameter("name", name)
+								.addParameter("kind", addressType)
 								.addParameter("street", street)
 								.addParameter("city", city)
 								.addParameter("state", state)
 								.addParameter("zip", zip)
 								.executeUpdate();
 					} else {
-						c.createQuery("UPDATE addresses SET name = :name, street = :street, city = :city, state = :state, zip = :zip WHERE username = :usr AND kind = 'HOME'")
+						c.createQuery("UPDATE addresses SET name = :name, street = :street, city = :city, state = :state, zip = :zip WHERE username = :usr AND kind = :kind")
 								.addParameter("usr", getCheesrSession().getUsername())
 								.addParameter("name", name)
+								.addParameter("kind", addressType)
 								.addParameter("street", street)
 								.addParameter("city", city)
 								.addParameter("state", state)
@@ -72,6 +89,7 @@ public final class ProfilePage extends CheesrPage implements IRequireAuthorizati
 		form.add(tfCity.setRequired(true));
 		form.add(tfState.setRequired(true));
 		form.add(tfZip.setRequired(true));
+		form.add(ddc.setRequired(true));
 
 		form.add(new Link("cancel") {
 			@Override
